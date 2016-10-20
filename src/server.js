db169348a2fc000510a7a69708b8b402d81e75ad
Gorka Hernandez <gorka.hernandez@datagenicgroup.com>
2016-10-20 12:44:58 +0200

    Update config.

--- src/server.js
+++ src/server.js
@@ -1,22 +1,21 @@
 import util from 'util';
 import stomp from 'stomp';
-import config from '../config/sample_config';
+import config from '../config/config';
 
-const client = new stomp.Stomp(config);
+const client = new stomp.Stomp(config),
+      headers = {
+          destination: config.topic,
+          ack: 'client'
+      };
 
-var headers = {
-    destination: config.topic,
-    ack: 'client'
-};
-
-var messages = 0;
+var msgCount = 0;
 
 console.log('Connecting to socket...');
 client.connect();
 
 function message_callback(body, headers) {
     console.log('|-- Message callback --|');
-    console.log('Headers: ' + sys.inspect(headers));
+    console.log('Headers: ' + util.inspect(headers));
     console.log('Body: ' + body);
 }
 
@@ -27,23 +26,20 @@ client.on('connected', () => {
 
 client.on('disconnected', () => {
     console.log('Disconnected from socket.');
-    console.log('Reattempting connection...');
-    client.connect();
 });
 
 client.on('message', function(message) {
     console.log("Got message: " + message.headers['message-id']);
     client.ack(message.headers['message-id']);
-    messages++;
+    msgCount++;
 });
 
 client.on('error', function(error_frame) {
     console.log('An error ocurred.');
     console.log(error_frame);
-    client.disconnect();
 });
 
 process.on('SIGINT', function() {
-    console.log('\nConsumed ' + messages + ' messages');
+    console.log(`Consumed ${msgCount} messages`);
     client.disconnect();
 });
